c9ca3efa7180aeef39d507dfef38bfbef81b9400
Silvia Martinez <201704856Q04856@est.umss.edu>
2022-11-04 19:15:02 -0400

    Mostrar imagen del negocio

--- src/screens/businessDetail/ShowBusinessDetail.jsx
+++ src/screens/businessDetail/ShowBusinessDetail.jsx
@@ -1,14 +1,36 @@
 import { StyleSheet, View } from 'react-native';
 import { useTheme } from 'react-native-paper';
-
+import logo from '../../../assets/picture.png';
 import BrowserLinking from '../../components/linking/';
 
+const API_HOST = 'https://blooming-inlet-07928.herokuapp.com';
+const business = {
+	name: 'Pollos campeón ',
+	category: 'restaurante-familiar',
+	location: 'https://maps.app.goo.gl/ssqhPPZfAd9NF5MN6',
+	id_business: '0A2K',
+	description: 'Pollos esquisitos ',
+};
 const ShowBusinessDetail = () => {
 	const { colors } = useTheme();
-
+	const [imageError, setImageError] = useState(false);
 	return (
 		<View style={{ ...styles.view, backgroundColor: colors.surface }}>
-			<BrowserLinking />
+			<View>
+				<Image
+					onError={() => {
+						setImageError(true);
+					}}
+					source={
+						imageError
+							? image
+							: {
+									uri: `${API_HOST}/product/${business.id_business}/image`,
+							  }
+					}
+					style={styles.image}
+				/>
+			</View>
 		</View>
 	);
 };
